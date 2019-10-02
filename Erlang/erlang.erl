1d12ac8cba4800b0087c53d733acf301029ba415
Jacky Hui <halojax@hotmail.com>
2019-10-02 21:53:29 +0100

    Added solution for Erlang.

---
+++ Erlang/erlang.erl
@@ -0,0 +1,30 @@
+-module(erlang).
+
+-export([main/1]).
+
+main(_Args) ->
+    {ok, File} = file:read_file("words.txt"),
+    Words = binary:split(File, <<"\n">>, [global, trim_all]),
+    Longest = find_longest_words(Words),
+    [io:format("~s~n", [Word]) || Word <- Longest].
+
+find_longest_words(Words) ->
+    Invalid = "gkmqvwxzio",
+    InvalidWithUppercase = Invalid ++ [C-32 || C <- Invalid],
+    Pattern = binary:compile_pattern([<<C>>|| C <- InvalidWithUppercase]),
+    find_longest_words(Words, Pattern, 0, []).
+
+find_longest_words([], _, _, Acc) ->
+    lists:reverse(Acc);
+find_longest_words([Word|RemainingWords], Pattern, MaxLen, Acc) ->
+    case {byte_size(Word), is_pattern_in_word(Word, Pattern)} of
+        {MaxLen, false} ->
+            find_longest_words(RemainingWords, Pattern, MaxLen, [Word|Acc]);
+        {GreaterLen, false} when GreaterLen > MaxLen ->
+            find_longest_words(RemainingWords, Pattern, GreaterLen, [Word]);
+        _ ->
+            find_longest_words(RemainingWords, Pattern, MaxLen, Acc)
+    end.
+
+is_pattern_in_word(Word, Pattern) ->
+    binary:matches(Word, Pattern) =/= [].
