deb6c649f254a2249a6a4fef9d4542f038547d69
ObserverOfTime <chronobserver@disroot.org>
2023-07-07 20:04:23 +0300

    Switch TypeScript implementation to Deno

--- JavaScript/TypeScript.ts
+++ JavaScript/TypeScript.ts
@@ -1,14 +1,15 @@
+import { readLines } from 'https://deno.land/std@0.193.0/io/mod.ts';
+
 const invalid = (s: string) => /[gkmqvwxzio]/i.test(s);
 let longest: string[] = [], maxlen: number = 0;
 
-require('readline').createInterface({
-    input: require('fs').createReadStream('words.txt')
-}).on('line', word => {
+for await (const word of readLines(await Deno.open('words.txt'))) {
     if (word.length === maxlen && !invalid(word)) {
         longest.push(word);
     } else if (word.length > maxlen && !invalid(word)) {
         longest = [word];
         maxlen = word.length;
     }
-}).on('close', () => console.log(longest.join('\n')));
+}
 
+console.log(longest.join('\n'));
