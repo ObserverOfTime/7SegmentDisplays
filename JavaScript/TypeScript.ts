2f4493f6975b0b7cd87ef64a9f18c1ff5c3250f7
ObserverOfTime <chronobserver@disroot.org>
2026-05-30 18:11:54 +0300

    Update Deno implementation

--- JavaScript/TypeScript.ts
+++ JavaScript/TypeScript.ts
@@ -1,15 +1,19 @@
-import { readLines } from 'https://deno.land/std@0.193.0/io/mod.ts';
+import { TextLineStream } from "jsr:@std/streams";
 
-const invalid = (s: string) => /[gkmqvwxzio]/i.test(s);
-let longest: string[] = [], maxlen: number = 0;
+const invalid = /[gkmqvwxzio]/i;
+let longest: string[] = [];
+let maxlen = 0;
 
-for await (const word of readLines(await Deno.open('words.txt'))) {
-    if (word.length === maxlen && !invalid(word)) {
-        longest.push(word);
-    } else if (word.length > maxlen && !invalid(word)) {
-        longest = [word];
-        maxlen = word.length;
-    }
+const words = (await Deno.open("words.txt")).readable
+    .pipeThrough(new TextDecoderStream()).pipeThrough(new TextLineStream());
+
+for await (const word of words) {
+  if (word.length === maxlen && !invalid.test(word)) {
+    longest.push(word);
+  } else if (word.length > maxlen && !invalid.test(word)) {
+    longest = [word];
+    maxlen = word.length;
+  }
 }
 
 console.log(longest.join('\n'));
